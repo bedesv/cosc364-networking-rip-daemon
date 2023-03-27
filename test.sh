a6de0e65a053c411bc2d0b7bd2140daf9b3e286d
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2023-03-28 12:48:20 +1300

    Added test.sh

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# gnome-terminal -e "python3 Daemon.py config_1.txt"
+# gnome-terminal -e "python3 Daemon.py config_2.txt"
+# gnome-terminal -e "python3 Daemon.py config_3.txt"
+# gnome-terminal -e "python3 Daemon.py config_4.txt"
+# gnome-terminal -e "python3 Daemon.py config_5.txt"
+# gnome-terminal -e "python3 Daemon.py config_6.txt"
+# gnome-terminal -e "python3 Daemon.py config_7.txt"
+
+
+gnome-terminal -e "python3 Daemon.py config1.txt"
+gnome-terminal -e "python3 Daemon.py config2.txt"
+gnome-terminal -e "python3 Daemon.py config3.txt"
